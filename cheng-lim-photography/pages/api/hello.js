b5af192fdc7a8d90d95ef5dd79160da085e90578
PACMAN-3\wenyo <wenyong.lim@icloud.com>
2023-01-14 19:29:40 +0000

    Changing to nextjs-blog

--- cheng-lim-photography/pages/api/hello.js
+++
@@ -1,5 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// Hello is an API Route Endpoint.
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' });
-}
