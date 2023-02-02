a4e45f6474ee510a4998c383c848d7f1cb08b590
PACMAN-3\wenyo <wenyong.lim@icloud.com>
2023-02-02 22:06:52 +0000

    Adding some Images

--- next-website/pages/index.jsx
+++ next-website/pages/index.jsx
@@ -2,14 +2,16 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Button, Text } from '@nextui-org/react';
 import NavBar from '@/components/NavBar';
+import test1 from '../public/1.png';
+import test2 from '../public/2.png';
 
 const images = [
   {
-    src: '../public/1.png',
+    src: { test1 },
     alt: 'Image 1',
   },
   {
-    src: '../public/1.png',
+    src: { test2 },
     alt: 'Image 2',
   },
 ];
