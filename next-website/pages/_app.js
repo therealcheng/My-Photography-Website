e03ecc02240c8008d658ae95a1e8fea132b79b3f
Cheng Lim <wenyong.lim@icloud.com>
2023-02-02 17:41:59 +0000

    Testing theming options in Next UI

--- next-website/pages/_app.js
+++ next-website/pages/_app.js
@@ -1,13 +1,43 @@
 // 1. import `NextUIProvider` component
-import { NextUIProvider } from '@nextui-org/react';
+import { NextUIProvider, createTheme } from '@nextui-org/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     // 2. Use at the root of your app
-    <NextUIProvider>
+    <NextUIProvider theme={theme}>
       <Component {...pageProps} />
     </NextUIProvider>
   );
 }
 
 export default MyApp;
+
+const theme = createTheme({
+  type: 'dark', // it could be "light" or "dark"
+  theme: {
+    colors: {
+      // brand colors
+      primaryLight: '$green200',
+      primaryLightHover: '$green300',
+      primaryLightActive: '$green400',
+      primaryLightContrast: '$green600',
+      primary: '#4ADE7B',
+      primaryBorder: '$green500',
+      primaryBorderHover: '$green600',
+      primarySolidHover: '$green700',
+      primarySolidContrast: '$white',
+      primaryShadow: '$green500',
+
+      gradient:
+        'linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)',
+      link: '#5E1DAD',
+
+      // you can also create your own color
+      myColor: '#ff4ecd',
+
+      // ...  more colors
+    },
+    space: {},
+    fonts: {},
+  },
+});
--- next-website/pages/index.js
+++ next-website/pages/index.js
@@ -1,12 +1,13 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { Button } from '@nextui-org/react';
+import { Button, Text } from '@nextui-org/react';
 
 export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
       <Button>Click me</Button>
+      <Text>NextUI colors</Text>
     </div>
   );
 }
