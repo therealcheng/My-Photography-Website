8b1fc0f335b4debe6c995a56eff460c40f5e1db4
PACMAN-3\wenyo <wenyong.lim@icloud.com>
2023-02-04 14:13:52 +0000

    Adding images and carousel to index

--- next-website/pages/index.jsx
+++ next-website/pages/index.jsx
@@ -2,24 +2,13 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Button, Text } from '@nextui-org/react';
 import NavBar from '@/components/NavBar';
-import test1 from '../public/1.png';
-import test2 from '../public/2.png';
-
-const images = [
-  {
-    src: { test1 },
-    alt: 'Image 1',
-  },
-  {
-    src: { test2 },
-    alt: 'Image 2',
-  },
-];
+import Carousel from '@/components/ImageCarousel';
 
 export default function Home() {
   return (
     <div>
       <NavBar />
+      <Carousel />
     </div>
   );
 }
