0cff03294ecb42a40cbc835e62e80f0f2c3863b6
PACMAN-3\wenyo <wenyong.lim@icloud.com>
2023-02-06 18:40:44 +0000

    Props for Carousel Bleed component

--- next-website/components/CarouselBleed.jsx
+++ next-website/components/CarouselBleed.jsx
@@ -1,30 +1,30 @@
 import test from '../public/test.jpg';
 import Image from 'next/image';
 
-const CarouselBleed = () => {
+const CarouselBleed = ({ image, widthSize, altText }) => {
   return (
     <div>
       <div className='carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box'>
         <div className='carousel-item'>
-          <Image alt='test' width={300} src={test} className='rounded-box' />
+          <Image alt='band' width={300} src={test} className='rounded-box' />
         </div>
         <div className='carousel-item'>
-          <Image alt='test' width={300} src={test} className='rounded-box' />
+          <Image alt='band' width={300} src={test} className='rounded-box' />
         </div>
         <div className='carousel-item'>
-          <Image alt='test' width={300} src={test} className='rounded-box' />
+          <Image alt='band' width={300} src={test} className='rounded-box' />
         </div>
         <div className='carousel-item'>
-          <Image alt='test' width={300} src={test} className='rounded-box' />
+          <Image alt='band' width={300} src={test} className='rounded-box' />
         </div>
         <div className='carousel-item'>
-          <Image alt='test' width={300} src={test} className='rounded-box' />
+          <Image alt='band' width={300} src={test} className='rounded-box' />
         </div>
         <div className='carousel-item'>
-          <Image alt='test' width={300} src={test} className='rounded-box' />
+          <Image alt='band' width={300} src={test} className='rounded-box' />
         </div>
         <div className='carousel-item'>
-          <Image alt='test' width={300} src={test} className='rounded-box' />
+          <Image alt='band' width={300} src={test} className='rounded-box' />
         </div>
       </div>
     </div>
