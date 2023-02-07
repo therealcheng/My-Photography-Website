1406d764103ec9e9b9711c14277c7f6c305531a0
PACMAN-3\wenyo <wenyong.lim@icloud.com>
2023-02-07 21:16:19 +0000

    Separating Image data to its own folder

---
+++ next-website/data/photos.jsx
@@ -0,0 +1,35 @@
+import img1 from '../public/1.jpg';
+import img2 from '../public/2.jpg';
+import img3 from '../public/3.jpg';
+import img4 from '../public/4.jpg';
+import img5 from '../public/5.jpg';
+
+const images = [
+  {
+    id: 1,
+    src: img1,
+    alt: 'Image 1',
+  },
+  {
+    id: 2,
+    src: img2,
+    alt: 'Image 2',
+  },
+  {
+    id: 3,
+    src: img3,
+    alt: 'Image 3',
+  },
+  {
+    id: 4,
+    src: img4,
+    alt: 'Image 4',
+  },
+  {
+    id: 5,
+    src: img5,
+    alt: 'Image 5',
+  },
+];
+
+export default images;
--- next-website/pages/index.jsx
+++ next-website/pages/index.jsx
@@ -4,39 +4,7 @@ import styles from '@/styles/Home.module.css';
 import NavbarNext from '@/components/NavbarNext';
 import Gallery from '@/components/Gallery';
 import test from '../public/test.jpg';
-import img1 from '../public/1.jpg';
-import img2 from '../public/2.jpg';
-import img3 from '../public/3.jpg';
-import img4 from '../public/4.jpg';
-import img5 from '../public/5.jpg';
-
-const images = [
-  {
-    id: 1,
-    src: img1,
-    alt: 'Image 1',
-  },
-  {
-    id: 2,
-    src: img2,
-    alt: 'Image 2',
-  },
-  {
-    id: 3,
-    src: img3,
-    alt: 'Image 3',
-  },
-  {
-    id: 4,
-    src: img4,
-    alt: 'Image 4',
-  },
-  {
-    id: 5,
-    src: img5,
-    alt: 'Image 5',
-  },
-];
+import images from '../data/photos';
 
 export default function Home() {
   return (
