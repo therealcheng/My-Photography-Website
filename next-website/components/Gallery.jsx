45d21a1564748a48246c9b2528b938deb486aa10
PACMAN-3\wenyo <wenyong.lim@icloud.com>
2023-02-07 20:05:09 +0000

    Adding 'Yet Another React Lightbox' to make lightbox on click

--- next-website/components/Gallery.jsx
+++ next-website/components/Gallery.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import Image from 'next/image';
+import Lightbox from 'yet-another-react-lightbox';
+import 'yet-another-react-lightbox/styles.css';
 
 const Gallery = ({ images }) => {
+  const [open, setOpen] = React.useState(false);
   return (
     <AnimatePresence>
       <div className='grid lg:grid-cols-3 lg:gap-1 p-8 gap-8'>
@@ -13,6 +16,7 @@ const Gallery = ({ images }) => {
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: 50 }}
             className='rounded-md overflow-hidden'
+            onClick={() => setOpen(true)}
           >
             <Image
               src={image.src}
@@ -20,6 +24,15 @@ const Gallery = ({ images }) => {
               width={800}
               height={500}
               className='object-cover'
+            />
+            <Lightbox
+              open={open}
+              close={() => setOpen(false)}
+              slides={[
+                { src: '/image1.jpg' },
+                { src: '/image2.jpg' },
+                { src: '/image3.jpg' },
+              ]}
             />
           </motion.div>
         ))}
