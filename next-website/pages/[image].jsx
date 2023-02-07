26bf08ca3f7b530d46929e8bb6b1965da6844c75
Cheng Lim <wenyong.lim@icloud.com>
2023-02-07 21:13:19 +0000

    Motion to [image].jsx div

--- next-website/pages/[image].jsx
+++ next-website/pages/[image].jsx
@@ -1,12 +1,17 @@
 // Map any Route with the url /home/[image] inside [image].js
 import { useRouter } from 'next/router';
+import { motion } from 'framer-motion';
 
 const fullScreenImage = () => {
   const router = useRouter(); // access product id route parameter
 
   // Rendering id as part of the jsx:
   const number = router.query.image;
-  return <div>Details about Image {number}</div>;
+  return (
+    <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
+      Details about Image {number}
+    </motion.div>
+  );
 };
 
 export default fullScreenImage;
