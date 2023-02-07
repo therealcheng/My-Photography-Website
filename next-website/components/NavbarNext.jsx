180e39f144fa5f135e59e8db9347307ee5e19637
PACMAN-3\wenyo <wenyong.lim@icloud.com>
2023-02-07 17:40:46 +0000

    Renaming nav text and optimised for responsiveness in small and large devices

--- next-website/components/NavbarNext.jsx
+++ next-website/components/NavbarNext.jsx
@@ -1,42 +1,44 @@
 import { Navbar, Button, Link, Text, useTheme } from '@nextui-org/react';
 import { Layout } from './Layout.jsx';
+import Image from 'next/image.js';
+import logo from '../public/logoLight.png';
 
 export default function NavbarNext() {
   const collapseItems = [
-    'Features',
-    'Customers',
-    'Pricing',
-    'Company',
-    'Legal',
-    'Team',
-    'Help & Feedback',
-    'Login',
-    'Sign Up',
+    'Home',
+    'Concert',
+    'Events',
+    'Lifestyle',
+    'Video',
+    'About',
   ];
   return (
     <Layout>
       <Navbar variant='sticky'>
         <Navbar.Brand>
-          <Navbar.Toggle aria-label='toggle navigation' />
+          <Navbar.Toggle aria-label='toggle navigation' className='lg:hidden' />
+          <Image src={logo} width={50} height={50} />
           <Text b color='inherit' hideIn='xs'>
-            ACME
+            CHENG LIM PHOTOGRAPHY
           </Text>
         </Navbar.Brand>
         <Navbar.Content enableCursorHighlight hideIn='xs' variant='underline'>
-          <Navbar.Link href='#'>Features</Navbar.Link>
-          <Navbar.Link isActive href='#'>
-            Customers
+          <Navbar.Link isActive href='/'>
+            Home
           </Navbar.Link>
-          <Navbar.Link href='#'>Pricing</Navbar.Link>
-          <Navbar.Link href='#'>Company</Navbar.Link>
+          <Navbar.Link href='#'>Concert</Navbar.Link>
+          <Navbar.Link href='#'>Events</Navbar.Link>
+          <Navbar.Link href='#'>Lifestyle</Navbar.Link>
+          <Navbar.Link href='#'>Video</Navbar.Link>
+          <Navbar.Link href='#'>About</Navbar.Link>
         </Navbar.Content>
         <Navbar.Content>
           <Navbar.Link color='inherit' href='#'>
-            Login
+            Instagram
           </Navbar.Link>
           <Navbar.Item>
             <Button auto flat as={Link} href='#'>
-              Sign Up
+              Contact
             </Button>
           </Navbar.Item>
         </Navbar.Content>
