2baca39cd7d2d5bf3b1e289097e4ec14e99574cd
PACMAN-3\wenyo <wenyong.lim@icloud.com>
2023-02-18 00:38:32 +0000

    Adding nav bar to contact page

--- next-website/pages/contact.jsx
+++ next-website/pages/contact.jsx
@@ -1,12 +1,17 @@
+import NavbarNext from '@/components/NavbarNext';
+
 const Contact = () => {
   return (
-    <div className='flex justify-center py-8'>
-      <input
-        type='text'
-        placeholder='Type here'
-        class='input input-bordered input-primary w-full max-w-xs'
-      />
-    </div>
+    <>
+      <NavbarNext />
+      <div className='flex justify-center py-8'>
+        <input
+          type='text'
+          placeholder='Type here'
+          class='input input-bordered input-primary w-full max-w-xs'
+        />
+      </div>
+    </>
   );
 };
 
